c75f11118eef214abf9873b5166a4a0cb0229bad
vipul patel <vipul1oovp@gmail.com>
2024-02-20 14:17:26 +0530

    Using HikariCP for connection pooling(logical connection)

--- src/in/ineuron/util/JdbcUtil.java
+++ src/in/ineuron/util/JdbcUtil.java
@@ -1,16 +1,15 @@
 package in.ineuron.util;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class JdbcUtil {
 	private static Connection connection = null;
-	private static Properties p = null;
-	private static FileInputStream fis = null;
+//	private static Properties p = null;
+//	private static FileInputStream fis = null;
 	
 	private JdbcUtil() {
 		
@@ -25,15 +24,21 @@ public class JdbcUtil {
 	}
 	
 	public static Connection getConnection() throws IOException, SQLException {
-		if(connection==null) {
-			fis = new FileInputStream("src\\in\\ineuron\\properties\\db.properties");
-			p = new Properties();
-			p.load(fis);
-			String url = p.getProperty("url");
-			String user = p.getProperty("user");
-			String password = p.getProperty("password");
-			connection = DriverManager.getConnection(url,user,password);
-		}
+		// using HikariCP for Connection Pooling.
+			
+		HikariConfig config = new HikariConfig("src\\in\\ineuron\\properties\\db.properties");
+		HikariDataSource dataSource = new HikariDataSource(config);
+		connection = dataSource.getConnection();
+			
 		return connection;
 	}
 }
+
+
+//fis = new FileInputStream("src\\in\\ineuron\\properties\\db.properties");
+//p = new Properties();
+//p.load(fis);
+//String url = p.getProperty("url");
+//String user = p.getProperty("user");
+//String password = p.getProperty("password");
+//connection = DriverManager.getConnection(url,user,password);
